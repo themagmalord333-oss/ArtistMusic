a273e727a06b73cbc72acaa23fb54d2efa6fdcc4
Artist <satyamshiv55@gmail.com>
2026-06-01 01:24:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,18 @@
+# ==========================================================
+# Copyright (c) 2026 ArtistBots
+# All Rights Reserved.
+#
+# Project      : ArtistBots API Telegram Music Bot
+# Powered By   : Artist
+# Type         : API Based Telegram Music Bot
+#
+# Bot          : @ArtistApibot
+# Channel      : https://t.me/artistbots
+# GitHub       : https://github.com/elevenyts
+#
+# Unauthorized copying, modification, or redistribution
+# of this source code without permission is prohibited.
+# ==========================================================
 from os import getenv
 from typing import List, Optional
 from dotenv import load_dotenv
