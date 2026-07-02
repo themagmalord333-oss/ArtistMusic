0fee87185fe342a5bcbdce7ff2bc3e77ef2d54e2
themagmalord333-oss <the.magma.lord.333@gmail.com>
2026-07-02 11:28:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -68,10 +68,10 @@ class Config:
         self.COOKIES_URL: List[str] = self._parse_cookies()
 
         # Images
-        self.DEFAULT_THUMB: str = getenv("DEFAULT_THUMB", "https://files.catbox.moe/zlmv6v.png")
-        self.PING_IMG: str = getenv("PING_IMG", "https://files.catbox.moe/zlmv6v.png")
-        self.START_IMG: str = getenv("START_IMG", "https://files.catbox.moe/zlmv6v.png")
-        self.RADIO_IMG: str = getenv("RADIO_IMG", "https://files.catbox.moe/zlmv6v.png")
+        self.DEFAULT_THUMB: str = getenv("DEFAULT_THUMB", "https://i.ibb.co/F4JRHBzr/x.jpg")
+        self.PING_IMG: str = getenv("PING_IMG", "https://i.ibb.co/F4JRHBzr/x.jpg")
+        self.START_IMG: str = getenv("START_IMG", "https://i.ibb.co/F4JRHBzr/x.jpg")
+        self.RADIO_IMG: str = getenv("RADIO_IMG", "https://i.ibb.co/F4JRHBzr/x.jpg")
 
         # Moderation
         self.EXCLUDED_USERNAMES: List[str] = getenv("EXCLUDED_USERNAMES", "").split()
